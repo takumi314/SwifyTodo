d359e17d00a8ba3dc03b68efa7b48d9295ee08b2
takumi314 <isospin_symmetry@me.com>
2016-10-16 12:07:26 +0900

    Add-Realm-Methods# Add fours methods for realm.write()

--- SwifyTodo/ViewController.swift
+++ SwifyTodo/ViewController.swift
@@ -7,12 +7,26 @@
 //
 
 import UIKit
+import RealmSwift
+
+/// Initialize the particular directory
+let paths  = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask , true)
+let path: String = paths[0] + "/swifyToDo.realm"
+let url: NSURL = NSURL(fileURLWithPath:path)
 
 class ViewController: UIViewController {
+    
+    // Public properties
+    let realm = try! Realm(fileURL: url)
+    
 
+    // Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        save()
+        findAll()
+        delete()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +34,53 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    // Public methods
+    
+    internal func save() -> Void {
 
+        let user = UserModel()
+        user.name = "Joan"
+        user.password = "gggg"
+        
+        do {
+            try realm.write() {
+                realm.add(user)
+            }
+        } catch {
+            print("Failed to save something is wrong!")
+        }
+        
+    }
+    
+    internal func findAll() -> Void {
+        // Load all the data from swifyToDo.realm.realm
+        let guys = realm.objects(UserModel)
+        //Show logs in data loaded from Realm
+        for guy in guys {
+            print("Username : \(guy)")
+        }
+    }
+
+    internal func delete() -> Void {
+        
+        do {
+            let data = realm.objects(UserModel).last!
+            
+            try realm.write() {
+                realm.delete(data)
+            }
+        } catch {
+            print("Failed to delete")
+        }
+        
+    }
+    
+    
+    // Private methods
+    
+    private func connectRealm() -> Void {
+    }
+    
 }
 
